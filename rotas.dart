2b3469a6057baf5c729e9cf27c0b4f46dc08a9a6
Wilker Nunes <51373362+wilkerdn@users.noreply.github.com>
2021-07-27 20:58:37 -0300

    Delete rotas.dart

--- rotas.dart
+++
@@ -1,8 +0,0 @@
-class Rotas {
-    static const AUTH = '/';
-  static const HOME = '/home';
-  static const TELA_MUSICAS = '/musicas';
-  static const FORM_ARTISTA = '/form_artista';
-  static const FORM_MUSICAS = '/form_musicas';
-  static const EDIT_MUSICAS = '/edit_musicas';
-}
